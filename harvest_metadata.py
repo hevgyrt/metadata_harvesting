cc99c749ac5abee869ba514c90bf6a69886208dd
trygveh <trygveh@met.no>
2017-01-30 14:45:43 +0100

    " Modified code and added resumptionToken"

--- harvest_metadata.py
+++ harvest_metadata.py
@@ -1,6 +1,11 @@
 """ Script for harvesting metadata
+    Inspired by harvest-metadata from https://github.com/steingod/mdharvest/tree/master/src
+    and code from http://lightonphiri.org/blog/metadata-harvesting-via-oai-pmh-using-python
 
 AUTOR: Trygve Halsne, 25.01.2017
+
+USAGE:
+    - input must have metadataPrefix?
 
 COMMENTS:
     - Implement it object oriented by means of classes
@@ -9,14 +14,15 @@ COMMENTS:
 
 # List all recordsets: http://arcticdata.met.no/metamod/oai?verb=ListRecords&set=nmdc&metadataPrefix=dif
 # List identifier: http://arcticdata.met.no/metamod/oai?verb=GetRecord&identifier=urn:x-wmo:md:no.met.arcticdata.test3::ADC_svim-oha-monthly&metadataPrefix=dif
+# Recordset with resumptionToken: http://union.ndltd.org/OAI-PMH/?verb=ListRecords&metadataPrefix=oai_dc
+# Recordset with DIF elements and resumptionToken (NB! Slow server..): http://ws.pangaea.de/oai/provider?verb=ListRecords&metadataPrefix=dif
+# Recordset with DIF elements and resumptionToken: https://esg.prototype.ucar.edu/oai/repository.htm?verb=ListRecords&metadataPrefix=dif
+
 import urllib2 as ul2
+import urllib as ul
 from xml.dom.minidom import parseString
 import codecs
 import sys
-
-sys.path.insert(0,'/home/trygveh/documents/nbs/file_conversion/checkMMD')
-from myCheckMMDFunctions import validKeywords
-
 
 global valid_protocols
 valid_protocols = ['OAI-PMH']
@@ -34,35 +40,123 @@ class HarvestMetadata(object):
     def harvest(self):
         baseURL, records, hProtocol = self.baseURL, self.records, self.hProtocol
         if hProtocol == 'OAI-PMH':
+            print "Harvesting metadata \n"
             getRecordsURL = str(baseURL + records)
-            harvestContent = ul2.urlopen(getRecordsURL)
-            data = harvestContent.read()
-            harvestContent.close()
-            #print data
-            #print '\n'
-            #print parseString(data).toxml()
-            output = codecs.open('test.xml','w')
-            output.write(data)
-            output.close()
+
+            # Initial phase
+            resumptionToken = self.oaipmh_resumptionToken(getRecordsURL)
+            dom = self.oaipmh_harvestContent(getRecordsURL)
+            if dom != None:
+                self.oaipmh_writeDIFtoFile(dom)
+            pageCounter = 1
+
+            while resumptionToken != []:
+                #print "\nHandeling resumptionToken: " + str(pageCounter)
+                resumptionToken = ul.urlencode({'resumptionToken':resumptionToken}) # create resumptionToken URL parameter
+                print resumptionToken
+                getRecordsURLLoop = str(baseURL+'?verb=ListRecords&'+resumptionToken)
+                dom = self.oaipmh_harvestContent(getRecordsURLLoop)
+                if dom != None:
+                    self.oaipmh_writeDIFtoFile(dom)
+                else:
+                    print "dom = " + str(dom) + ', for page ' + str(pageCounter)
+
+                resumptionToken = self.oaipmh_resumptionToken(getRecordsURLLoop)
+                pageCounter += 1
+
+            return dom
+
         else:
             print 'Protocol %s is not accepted.' % hProtocol
             sys.exit()
-        '''
-        if not validKeywords([hProtocol],valid_protocols):
-            print 'Protocol %s is not accepted.' % hProtocol
-            sys.exit()
-        '''
+
+    def oaipmh_writeDIFtoFile(self,dom):
+        """ Write DIF elements in dom to file """
+        print "Writing DIF elements to disk"
+
+        record_elements = dom.getElementsByTagName('record')
+        size_dif = dom.getElementsByTagName('DIF').length
+
+        if size_dif != 0:
+            counter = 0
+
+            for record in record_elements:
+                for child in record.childNodes:
+                    if str(child.nodeName) == 'header':
+                        has_attrib = child.hasAttributes()
+                        for gchild in child.childNodes:
+                            if gchild.nodeName == 'identifier':
+                                id_text = gchild.childNodes[0].nodeValue
+                                break;
+
+                if not has_attrib:
+                    sys.stdout.write('Extracting %.f / %d DIF elements \r' %(counter,size_dif))
+                    sys.stdout.flush()
+                    dif = record.getElementsByTagName('DIF')[0]
+                    #tmp_fname ='dif_test_' + str(id_text) + '.xml'
+                    tmp_fname ='dif_test_' + str(counter) + '.xml'
+                    output = codecs.open(tmp_fname ,'w','utf-8')
+                    dif.writexml(output)
+                    output.close()
+                    counter += 1
+                if counter == 2:
+                    break;
+        else:
+            print "records did not contain DIF elements"
+
+    def oaipmh_harvestContent(self,URL):
+        try:
+            file = ul2.urlopen(URL,timeout=40)
+            data = file.read()
+            file.close()
+            return parseString(data)
+        except ul2.HTTPError:
+            print "There was an error with the request"
+
+    def oaipmh_resumptionToken(self,URL):
+        try:
+            file = ul2.urlopen(URL, timeout=40)
+            data = file.read()
+            file.close()
+            dom = parseString(data)
+
+            if dom.getElementsByTagName('resumptionToken').length == 0:
+                return dom.getElementsByTagName('resumptionToken')
+            else:
+                if dom.getElementsByTagName('resumptionToken')[0].firstChild != None:
+                    return dom.getElementsByTagName('resumptionToken')[0].firstChild.nodeValue
+                else:
+                    return []
+        except ul2.HTTPError:
+            print "There was an error with the request"
 
 
+baseURL = 'https://esg.prototype.ucar.edu/oai/repository.htm'
+records = '?verb=ListRecords&metadataPrefix=dif'
+outputDir = 'tmp'
+hProtocol = 'OAI-PMH'
+
+hm = HarvestMetadata(baseURL,records, outputDir, hProtocol)
+content = hm.harvest()
+
+'''
+hm = HarvestMetadata(baseURL='http://arcticdata.met.no/metamod/oai',
+                      records='?verb=ListRecords&set=nmdc&metadataPrefix=dif',
+                      outputDir='tmp', hProtocol='OAI-PMH')
+content = hm.harvest()
+
+'''
+
+"""
 def main():
     hm = HarvestMetadata(baseURL='http://arcticdata.met.no/metamod/oai',
                           records='?verb=GetRecord&identifier=urn:x-wmo:md:no.met.arcticdata.test3::ADC_svim-oha-monthly&metadataPrefix=dif',
                           outputDir='tmp', hProtocol='OAI-PMH')
-    hm.harvest()
-
+    content = hm.harvest()
+    print content
 if __name__ == '__main__':
     main()
-
+"""
 
 
 """
