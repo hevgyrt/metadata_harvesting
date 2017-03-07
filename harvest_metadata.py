7915eececdadd82c7cb3574d7c221e6e1df172c1
trygveh <trygveh@met.no>
2017-03-07 13:26:59 +0100

    -

--- harvest_metadata.py
+++ harvest_metadata.py
@@ -3,10 +3,10 @@
         - harvest-metadata from https://github.com/steingod/mdharvest/tree/master/src
         - code from http://lightonphiri.org/blog/metadata-harvesting-via-oai-pmh-using-python
 
-AUTOR: Trygve Halsne, 25.01.2017
+AUTHOR: Trygve Halsne, 25.01.2017
 
 USAGE:
-    - input must have metadataPrefix?
+    - Currently initiaded with internal methods in class
 
 COMMENTS:
     - Implement it object oriented by means of classes
@@ -14,16 +14,7 @@ COMMENTS:
     - Does OGC-CSW metadata have some kind of resumptionToken analog?
     - Should/must implement with a "from_date" variable in order to avoid listing of same metadata
 """
-# List all recordsets: http://arcticdata.met.no/metamod/oai?verb=ListRecords&set=nmdc&metadataPrefix=dif
-# List identifier: http://arcticdata.met.no/metamod/oai?verb=GetRecord&identifier=urn:x-wmo:md:no.met.arcticdata.test3::ADC_svim-oha-monthly&metadataPrefix=dif
-# Recordset with resumptionToken: http://union.ndltd.org/OAI-PMH/?verb=ListRecords&metadataPrefix=oai_dc
-# Recordset with DIF elements and resumptionToken (Slow server..): http://ws.pangaea.de/oai/provider?verb=ListRecords&metadataPrefix=dif
-# Recordset with DIF elements and resumptionToken: https://esg.prototype.ucar.edu/oai/repository.htm?verb=ListRecords&metadataPrefix=dif
-# Recordset with gcmd(DIF) elements: http://oai.nerc-bas.ac.uk:8080/oai/provider?verb=ListRecords&metadataPrefix=gcmd
 
-# OGC-CSW recordset: http://metadata.bgs.ac.uk/geonetwork/srv/en/csw?SERVICE=CSW&VERSION=2.0.2&request=GetRecords&constraintLanguage=CQL_TEXT&typeNames=csw:Record&resultType=results&outputSchema=http://www.isotc211.org/2005/gmd
-
-# OpenSearch from sentinel scihub: https://scihub.copernicus.eu/dhus/search?q=S2A*
 
 import urllib2 as ul2
 import urllib as ul
@@ -35,18 +26,22 @@ import lxml.etree as ET
 
 
 class MetadataHarvester(object):
-    def __init__(self, baseURL, records, outputDir, hProtocol): # add outputname also?
+    """ Creates metadata-harvester object with methods for harvesting and writing
+    """
+    def __init__(self, baseURL, records, outputDir, hProtocol, username=None, pw=None):
         """ set variables in class """
         self.baseURL = baseURL
         self.records = records
         self.outputDir = outputDir
         self.hProtocol = hProtocol
+        self.username = username
+        self.pw = pw
 
     def harvest(self):
         """ Inititates harvester. Chooses strategy depending on
             harvesting  protocol
         """
-        baseURL, records, hProtocol = self.baseURL, self.records, self.hProtocol
+        baseURL, records, hProtocol, uname, pw = self.baseURL, self.records, self.hProtocol, self.username, self.pw
 
         if hProtocol == 'OAI-PMH':
             # Could/should be more sophistiated by means of deciding url properties
@@ -86,6 +81,7 @@ class MetadataHarvester(object):
                 self.ogccsw_writeCSWISOtoFile(dom)
 
             print "\n\nHarvesting took: %s [h:mm:ss]\n" % str(datetime.now()-start_time)
+
         elif hProtocol == "OpenSearch":
             getRecordsURL = str(baseURL + records)
             print "Harvesting metadata from: \n\tURL: %s \n\tprotocol: %s \n" % (getRecordsURL,hProtocol)
@@ -113,7 +109,7 @@ class MetadataHarvester(object):
                 print "\n\n\tHandling results (%s - %s) / %s" %(current_results, current_results + itemsPerPage, totalResults)
                 from_to = "?start=%s&rows=%s&" % (current_results,itemsPerPage)
                 getRecordsURLLoop = str(baseURL + from_to + records[1:])
-                dom = self.harvestContent(getRecordsURLLoop,credentials=True,uname="evgyrt",pw="a test user passphrase")
+                dom = self.harvestContent(getRecordsURLLoop,credentials=True,uname=uname,pw=pw)
                 if dom != None:
                     self.openSearch_writeENTRYtoFile(dom)
                 current_results += itemsPerPage
@@ -268,9 +264,6 @@ class MetadataHarvester(object):
             print "There was an error with the URL request"
 
 
-#baseURL = 'https://esg.prototype.ucar.edu/oai/repository.htm'
-#records = '?verb=ListRecords&metadataPrefix=dif'
-
 def main():
     """
     baseURL = 'http://oai.nerc-bas.ac.uk:8080/oai/provider'
@@ -289,19 +282,28 @@ def main():
     mh2 = MetadataHarvester(baseURL,records, outputDir, hProtocol)
     mh2.harvest()
     """
+
+    with open('myValues.txt','r') as code:
+        cred_tmp = code.readline()
+
+    cred = cred_tmp.split(';')
     baseURL = 'https://colhub.met.no/search'
     records = '?q=S2A*'
-    records = '?q=platformname:Sentinel-2%20AND%20ingestionDate:[NOW-3DAY%20TO%20NOW]'
+    records = '?q=platformname:Sentinel-1%20AND%20ingestionDate:[NOW-2DAY%20TO%20NOW]'
     outputDir = 'output/'
     hProtocol = 'OpenSearch'
-    mh3 = MetadataHarvester(baseURL,records, outputDir, hProtocol)
+    mh3 = MetadataHarvester(baseURL,records, outputDir, hProtocol,cred[0],cred[1])
     mh3.harvest()
 
 if __name__ == '__main__':
     main()
 
-#baseURL =  'http://dalspace.library.dal.ca:8080/oai/request'
-#arguments = '?verb=ListRecords&metadataPrefix=oai_dc'
-
-#baseURL = 'http://union.ndltd.org/OAI-PMH/'
-#getRecordsURL = str(baseURL+'?verb=ListRecords&metadataPrefix=oai_dc')
+# TEMPORARY VALUES
+# List all recordsets: http://arcticdata.met.no/metamod/oai?verb=ListRecords&set=nmdc&metadataPrefix=dif
+# List identifier: http://arcticdata.met.no/metamod/oai?verb=GetRecord&identifier=urn:x-wmo:md:no.met.arcticdata.test3::ADC_svim-oha-monthly&metadataPrefix=dif
+# Recordset with resumptionToken: http://union.ndltd.org/OAI-PMH/?verb=ListRecords&metadataPrefix=oai_dc
+# Recordset with DIF elements and resumptionToken (Slow server..): http://ws.pangaea.de/oai/provider?verb=ListRecords&metadataPrefix=dif
+# Recordset with DIF elements and resumptionToken: https://esg.prototype.ucar.edu/oai/repository.htm?verb=ListRecords&metadataPrefix=dif
+# Recordset with gcmd(DIF) elements: http://oai.nerc-bas.ac.uk:8080/oai/provider?verb=ListRecords&metadataPrefix=gcmd
+# OGC-CSW recordset: http://metadata.bgs.ac.uk/geonetwork/srv/en/csw?SERVICE=CSW&VERSION=2.0.2&request=GetRecords&constraintLanguage=CQL_TEXT&typeNames=csw:Record&resultType=results&outputSchema=http://www.isotc211.org/2005/gmd
+# OpenSearch from sentinel scihub: https://scihub.copernicus.eu/dhus/search?q=S2A*
